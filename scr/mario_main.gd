158bc261e800b3e8fa93d9f37ddaa418315e5c3a
toby" radiation "fox <166326616+radishcatish@users.noreply.github.com>
2026-03-19 16:56:15 -0400

    moer stuff

--- scr/mario_main.gd
+++ scr/mario_main.gd
@@ -17,7 +17,7 @@ func _physics_process(_d):
 	last_off_floor = 0 if not is_on_floor() else last_off_floor + 1
 	last_on_wall = 0 if is_on_wall_only() else last_on_wall + 1
 	last_wall_normal = get_wall_normal().x as int if is_on_wall_only() else last_wall_normal
-	dived_midair = false if is_on_floor() else dived_midair
+	dived_midair = false if is_on_floor() and not state == PlayerState.DIVE else dived_midair
 	kicked_midair = false if is_on_floor() else kicked_midair
 	spun_midair = false if is_on_floor() else spun_midair
 	
@@ -35,26 +35,28 @@ func _physics_process(_d):
 			sprite.snd_kick.play()
 	
 	if I.last_x_press == 1:
-		if I.d.y != 0 and not spun_midair:
+		if not is_on_floor() or I.d.y:
+			if not dived_midair and not I.d.x == 0:
+				dived_midair = true
+				sprite.snd_whoosh.play()
+				state = PlayerState.DIVE
+				sprite.play("dive")
+				velocity.y = I.d.y * -200
+				velocity.x = 350 * sprite.dir
+			elif not kicked_midair:
+				kicked_midair = true
+				sprite.snd_kick.play()
+				state = PlayerState.KICK
+				sprite.play("kick")
+				velocity.y = -300
+		elif not spun_midair:
 			spun_midair = true
 			sprite.snd_spinjump.play()
 			state = PlayerState.SPINJUMP
 			sprite.play("spin")
 			velocity.y = -350 if is_on_floor() else -200
-		elif (I.d.x == 0 or is_on_floor()) and not kicked_midair:
-			kicked_midair = true
-			sprite.snd_kick.play()
-			state = PlayerState.KICK
-			sprite.play("kick")
-			velocity.y = -300
-		elif not is_on_floor() and not dived_midair:
-			dived_midair = true
-			sprite.snd_whoosh.play()
-			state = PlayerState.DIVE
-			sprite.play("dive")
-			velocity.y = -200
-			velocity.x = 350 * sprite.dir
-			
+
+
 	velocity.y += 20 - int(I.z_pressed and velocity.y < 0) * 5
 	if not state == PlayerState.DIVE:
 		var target_speed = I.d.x * 300 if I.shift_pressed else I.d.x * 150
@@ -68,9 +70,8 @@ func _physics_process(_d):
 			
 	move_and_slide()
 
-	if is_on_wall_only() and state == PlayerState.DIVE:
-		state = PlayerState.GENERAL
-	if is_on_floor() and state == PlayerState.SPINJUMP:
+	if (is_on_floor() or is_on_wall_only()) and state == PlayerState.SPINJUMP \
+		or is_on_wall_only() and state == PlayerState.DIVE:
 		state = PlayerState.GENERAL
 
 func sprite_anim_finish() -> void:
